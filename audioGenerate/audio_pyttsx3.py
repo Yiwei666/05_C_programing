8fc1879939a08792f285073e9c7cd4a1c8908c39
Yiwei666 <69843201+Yiwei666@users.noreply.github.com>
2023-06-07 11:13:25 +0800

    Update audio_pyttsx3.py
    对之前代码进行简化

--- audioGenerate/audio_pyttsx3.py
+++ audioGenerate/audio_pyttsx3.py
@@ -8,29 +8,12 @@ Created on Wed Jun  7 10:12:19 2023
 import pyttsx3
 import time
 
-# 创建pyttsx3引擎
-engine = pyttsx3.init()
-
-sleptim = 1.4
-
-# 设置语言为中文
-engine.setProperty("voice", "zh")
-
-# 设置语音播放速度
-engine.setProperty("rate", 150)  # 调整播放速度，可以尝试不同的值
-
-text = input("请输入要转换为语音的文本：")
-
-# 将文本转换为语音
-engine.say(text)
-
-# 开始转换文本到语音
-engine.runAndWait()
-
-time.sleep(sleptim)
+text = input("Enter some text: ")
 
 # 循环播放语音
 while True:
+    sleptim = 1.3
+    
     # 创建新的pyttsx3引擎
     engine = pyttsx3.init()
 
